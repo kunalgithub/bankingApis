50f0fab7d59ba8af40bbd3458ce67e752ae635df
kunalgithub <kunal.kishan@gmail.com>
2018-09-30 23:06:23 +0530

    Changed the fileName and Handler Name

--- createSession.js
+++ createSession.js
@@ -1,5 +1,5 @@
 const https = require("got");
-exports.accounts = function (event, context, callback) {
+exports.createSession = function (event, context, callback) {
     https('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }).then(response => {
         console.log(response);
         callback(null, "");
