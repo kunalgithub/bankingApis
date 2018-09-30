7ea3521a7ac9085d4ad852e217582d7e5ae5ba76
kunalgithub <kunal.kishan@gmail.com>
2018-09-30 23:42:31 +0530

    Lamda session

--- session/index.js
+++ session/index.js
@@ -1,1 +1,16 @@
 
+const https = require("got");
+const uuidv1 = require('uuid/v1');
+
+exports.createSession = function (event, context, callback) {
+    var sessionResp={"sessionId": "string"};
+    sessionResp.sessionId=uuidv1();
+    console.log("Generating the SessionId",sessionResp.sessionId);
+    https('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }).then(response => {
+        console.log(response);
+        callback(null, sessionResp);
+    }).catch(error => {
+        console.log(error.response.body);
+        callback("Error in Accounts");
+    });
+}
